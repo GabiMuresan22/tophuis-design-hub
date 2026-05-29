56b4b6d5d3663124ce6f68f81a87ea5c669c8f50
Gabi <gabimuresan2289@gmail.com>
2026-05-29 19:58:25 +0200

    Reduce contact page map footprint

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -112,17 +112,21 @@ export default function Contact() {
       </section>
 
       {/* Map */}
-      <section className="h-96">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40154.07438849!2d3.5707!3d51.1167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c37b4e3bb9c9e7%3A0x1f0e9d7c99f3b3f0!2s9920%20Lievegem%2C%20Belgium!5e0!3m2!1sen!2sus!4v1635000000000!5m2!1sen!2sus"
-          width="100%"
-          height="100%"
-          style={{ border: 0 }}
-          allowFullScreen
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          title={t("TopHuis Locatie", "TopHuis Location")}
-        />
+      <section className="pb-16">
+        <div className="container-wide">
+          <div className="mx-auto h-80 max-w-5xl overflow-hidden rounded shadow-architect">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40154.07438849!2d3.5707!3d51.1167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c37b4e3bb9c9e7%3A0x1f0e9d7c99f3b3f0!2s9920%20Lievegem%2C%20Belgium!5e0!3m2!1sen!2sus!4v1635000000000!5m2!1sen!2sus"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              title={t("TopHuis Locatie", "TopHuis Location")}
+            />
+          </div>
+        </div>
       </section>
     </Layout>
   );
